bc888b79fae92c24457366a0ac4f75c15d405253
unknown <milanzec53@gmail.com>
2023-06-15 17:26:07 +0200

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+import Square from "./components/Square";
+
+export default function Board() {
+  const [square, setSquare] = useState(Array(9).fill(null));
+  const [nextIsX, setNextIsX] = useState(true);
+
+  function handleClick(i) {
+    const nextSquare = square.slice();
+    if (nextIsX) {
+      nextSquare[i] = "X";
+    } else {
+      nextSquare[i] = "O";
+    }
+    setSquare(nextSquare);
+    setNextIsX(!nextIsX);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="board-row">
+        <Square value={square[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={square[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={square[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+
+      <div className="board-row">
+        <Square value={square[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={square[5]} onSquareClick={() => handleClick(5)} />
+        <Square value={square[6]} onSquareClick={() => handleClick(6)} />
+      </div>
+      <div className="board-row">
+        <Square value={square[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={square[8]} onSquareClick={() => handleClick(8)} />
+        <Square value={square[9]} onSquareClick={() => handleClick(9)} />
+      </div>
+    </>
   );
 }
-
-export default App;
---
+++ src/components/Square.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import "../App.css";
+
+const Square = ({ value, onSquareClick }) => {
+  return (
+    <div>
+      <button onClick={onSquareClick} className="square">{value}</button>
+    </div>
+  );
+};
+
+export default Square;
