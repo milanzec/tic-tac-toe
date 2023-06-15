2a05363d3e466f8ce406fa549ee5f51ae41c9107
unknown <milanzec53@gmail.com>
2023-06-15 18:30:33 +0200

    loop fixed

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ export default function Board() {
   const [nextIsX, setNextIsX] = useState(true);
 
   function handleClick(i) {
+    if (square[i] || calculateWinner(square)) {
+      return;
+    }
     const nextSquare = square.slice();
     if (nextIsX) {
       nextSquare[i] = "X";
@@ -18,8 +21,17 @@ export default function Board() {
     setNextIsX(!nextIsX);
   }
 
+  const winner = calculateWinner(square);
+  let status;
+  if (winner) {
+    status = "Winner is " + winner;
+  } else {
+    status = "Next player is " + (nextIsX ? "X" : "O");
+  }
+
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={square[0]} onSquareClick={() => handleClick(0)} />
         <Square value={square[1]} onSquareClick={() => handleClick(1)} />
@@ -27,15 +39,38 @@ export default function Board() {
       </div>
 
       <div className="board-row">
+        <Square value={square[3]} onSquareClick={() => handleClick(3)} />
         <Square value={square[4]} onSquareClick={() => handleClick(4)} />
         <Square value={square[5]} onSquareClick={() => handleClick(5)} />
-        <Square value={square[6]} onSquareClick={() => handleClick(6)} />
       </div>
       <div className="board-row">
+        <Square value={square[6]} onSquareClick={() => handleClick(6)} />
         <Square value={square[7]} onSquareClick={() => handleClick(7)} />
         <Square value={square[8]} onSquareClick={() => handleClick(8)} />
-        <Square value={square[9]} onSquareClick={() => handleClick(9)} />
       </div>
     </>
   );
 }
+
+function calculateWinner(square) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (square[a] && square[a] === square[b] && square[a] === square[c]) {
+      return square[a];
+    }
+  }
+
+  return null;
+}
+
